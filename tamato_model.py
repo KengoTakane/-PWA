8d4f1f1086dd59e301cf851efea3dae3c623959d
KengoTakane <97824629+KengoTakane@users.noreply.github.com>
2022-11-22 15:50:11 +0900

    Add files via upload

---
+++ tamato_model.py
@@ -0,0 +1,60 @@
+from cProfile import label
+from re import T
+import numpy as np
+import statsmodels.api as sm
+from scipy import linalg, optimize
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+H_0 = 62.9
+H_plusinf = 43.1
+H_minusinf = 124
+k_ref = 0.0019
+E_a = 170.604
+T_ref = 288.15
+Rg = 0.008314
+
+
+def k(T):
+    return k_ref*np.exp((E_a/Rg)*(1/T_ref-1/T))
+
+
+def H(t,T):
+    return H_plusinf + (H_minusinf-H_plusinf)/(1+np.exp((k(T)*t)*(H_minusinf-H_plusinf))*(H_minusinf-H_0)/(H_0-H_plusinf))
+
+
+# H()のグラフ化
+sns.set()
+x = np.linspace(0,20,100)
+fig1 = plt.figure()
+ax = fig1.add_subplot(111)
+ax.plot(x,H(x,12+273),label="$12^{\circ}C$")
+ax.plot(x,H(x,15+273),label="$15^{\circ}C$")
+ax.plot(x,H(x,18+273),label="$18^{\circ}C$")
+ax.set_xlabel('time(days)')
+ax.set_ylabel('Hue(${}^\circ$)')
+ax.set_title('Quest')
+plt.legend(loc='best')
+sns.despine()
+
+Tem = np.linspace(10+273,20+273,100)
+fig2 = plt.figure()
+ax = fig2.add_subplot(111)
+ax.plot(Tem,k(Tem),label="$k(T)$")
+ax.set_xlabel('Temperature(K)')
+ax.set_ylabel('$k(T)$)')
+plt.legend(loc='best')
+
+fig3 = plt.figure()
+ax = fig3.add_subplot(111)
+ax.plot(Tem,-1/Tem,label="$-1/T$")
+ax.set_xlabel('Temperature(K)')
+ax.set_ylabel('$-1/T$')
+plt.legend(loc='best')
+
+plt.show()
+
+
+
+
