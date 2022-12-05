eec2f3bc8d4da2cf39b00e305134224b68f4cbce
Kengo Takane <takakengo2005@gmail.com>
2022-12-05 22:31:02 +0900

    2nd commit

--- tomato_pwa_model.py
+++ tomato_pwa_model.py
@@ -35,3 +35,40 @@ s = 3
 T = np.random.randint(T_min,T_max,(N,))
 H = np.random.randint(H_min,H_max,(N,))
 Enz = np.random.randint(Enz_min,Enz_max,(N,))
+
+# X : n×N dimension matrix, y : N dimension row vector(1-dim array)
+X = np.array([T,H,Enz])
+y_H = f_H(T,H,Enz)
+y_Enz = f_Enz(T,H,Enz)
+
+# Norm  :N×N dimension matrix, Norm_sort : N×N dimension matrix, C : N×c dimension matrix
+Norm = np.empty((X.shape[1], X.shape[1]))
+for i in range(X.shape[1]):
+    for j in range(X.shape[1]):
+        Norm[i, j] = np.linalg.norm(X[:, i] - X[:,j])
+Norm_sort = np.argsort(Norm, axis=1)
+C = Norm_sort[:, 0:c]
+
+# Xc : N×n×c dimension matrix, T_c,H_c,Enz_c : N×c dimension matrix, Yc : N×c×1 dimension matrix
+Xc = np.empty((C.shape[0], X.shape[0], C.shape[1]))
+for i in range(Xc.shape[0]):
+    for j in range(Xc.shape[2]):
+        Xc[i, :, j] = X[:, C[i, j]]
+T_c = Xc[:, 0, :]
+H_c = Xc[:, 1, :]
+Enz_c = Xc[:, 2, :]
+
+yc_H = f_H(T_c,H_c,Enz_c)
+Yc_H = yc_H[:, :, np.newaxis]
+yc_Enz = f_Enz(T_c,H_c,Enz_c)
+Yc_Enz = yc_Enz[:, :, np.newaxis]
+
+# Phi : N×c×(n+1) dimension matrix, (Phi)'×Phi : N×2×2 dimension matrix, inverse matrix of phi : N×c×c dimension matrix, theta_ls : N×(n+1)×1 dimension matrix
+one = np.ones((Xc.shape[0], 1, Xc.shape[2]))
+Phi_T = np.concatenate((Xc, one), axis=1)
+Phi = Phi_T.transpose(0, 2, 1)
+phi = Phi_T @ Phi
+inv_phi = np.linalg.inv(phi)
+PHI = inv_phi @ Phi_T
+Theta_ls_H = PHI @ Yc_H
+Theta_ls_Enz = PHI @ Yc_Enz
